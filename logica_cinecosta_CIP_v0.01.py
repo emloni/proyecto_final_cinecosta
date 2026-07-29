49fa743b63f766ffd812872d74f32604f0ffe744
emloni <emloniloneone@gmail.com>
2026-07-29 17:56:55 -0500

    Update logica_cinecosta_CIP_v0.01.py

--- logica_cinecosta_CIP_v0.01.py
+++ logica_cinecosta_CIP_v0.01.py
@@ -22,23 +22,28 @@ class teatro:
         self.ciudad = ciudad
         self.salas = [] # Agrupa las salas de este teatro
         
-class empresa:
+class Empresa:
     def __init__(self, nombre_empresa):
         self.nombre_empresa = nombre_empresa
+        self.lectura_archivo = None
         self.teatros = []
+        
 
     def leer_archivo(self, ruta_archivo):
         with open(ruta_archivo, mode="r", encoding="utf-8" ) as archivo:
             lector = csv.DictReader(archivo)
-            for filas in lector:
-                    nombre = filas["nombre_teatro"]
-                    ciudad = filas["ciudad"]
-                    
-                    teatro_existe = False
-                    
-                    for t in self.teatros:
-                        if t.nombre_teatro == nombre and t.ciudad == ciudad:
-                            teatro_existe = True
-                            break
-                    if not teatro_existe:
-                        self.teatros.append(teatro(nombre, ciudad)) 
+            self.lectura_archivo = list(lector)
+    
+    def agregar_teatro(self):
+        for fila in self.lectura_archivo:
+            nombre = fila["nombre_teatro"]
+            ciudad = fila["ciudad"]
+            
+            teatro_existe = False
+            
+            for t in self.teatros:
+                if t.nombre_teatro == nombre and t.ciudad == ciudad:
+                    teatro_existe = True
+                    break
+            if not teatro_existe:
+                self.teatros.append(Teatro(nombre, ciudad))
