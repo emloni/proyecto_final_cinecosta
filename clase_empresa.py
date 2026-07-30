a5cf937e5dd09ea48a4faaf72a00ed82881bbb03
emloni <emloniloneone@gmail.com>
2026-07-30 09:56:15 -0500

    Add files via upload

--- clase_empresa.py
+++ clase_empresa.py
@@ -1,17 +1,24 @@
-from logica_cinecosta_CIP import Teatro,Sala
+from sala_HU_4 import Sala
+from Logica_teatro import Teatro
 import csv
 
 class Empresa:
-    def __init__(self, nombre_empresa):
-        self.nombre_empresa = nombre_empresa
-        self.lectura_archivo = None
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.regristros = None
         self.teatros = []
         
 
     def leer_archivo(self, ruta_archivo):
-        with open(ruta_archivo, mode="r", encoding="utf-8" ) as archivo:
-            lector = csv.DictReader(archivo)
-            self.lectura_archivo = list(lector)
+        try:
+            with open(ruta_archivo, mode="r", encoding="utf-8" ) as archivo:
+                lector = csv.DictReader(archivo)
+                self.lectura_archivo = list(lector)
+        except OSError:
+            raise FileNotFoundError("No se pudo abrir el archivo.")
+        except ValueError:
+            raise ValueError("Archivo con formato no válido.")
+
     
     def agregar_teatro(self):
         for fila in self.lectura_archivo:
@@ -26,3 +33,16 @@ class Empresa:
                     break
             if not teatro_existe:
                 self.teatros.append(Teatro(nombre, ciudad))
+
+    def cargar_salas(self):
+        for fila in self.lectura_archivo:
+            nombre_teatro = fila["nombre_teatro"]
+            nombre_sala = fila["nombre_sala"]
+            tamaño_sala = fila["tamaño_sala"]
+            precio_base = fila["precio_base"]
+                
+            if precio_base <= 0:
+                raise ValueError("el precio base debe ser un valor numerico mayor a cero")
+            for t in self.teatros:
+                if t.nombre_teatro == nombre_teatro:
+                    t.salas.append(Sala(nombre_sala, tamaño_sala, precio_base))
